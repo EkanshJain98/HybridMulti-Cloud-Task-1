a4d8e7bac6cf2ebd5309b6be31ee5424fc1e291a
Ekansh jain <40785361+technicalej@users.noreply.github.com>
2020-06-14 00:57:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <html>
+<body>
+<h1>EKANSH JAIN</h1>
+<h3>KING OG BLOGGING</h3>
 
-
-<p>hello this is my terraform html based file</p>
+<p>hello this is my terraform PHP based file</p>
 <br>
 <?php
   $cloudfront = "head -n1 mydesti.txt";
   $img_path = "https://"./$cloudfront."/spider_man.jpg";
+  echo "<br>";
   echo "<img src='${img_path}' width=100 height=100>";
 ?>
-    </html>
+</body>
+</html>
