8078916fc4f28d9906a1b112eeb5a52f54e5174e
Ekansh jain <40785361+technicalej@users.noreply.github.com>
2020-06-14 00:41:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <br>
 <?php
   $cloudfront = "head -n1 mydesti.txt";
-  $img_path = "https://"./$cloudfront_url."/spider_man.jpg";
-  echo "<img src='${img_path}' width="100" height="100">";
+  $img_path = "https://"./$cloudfront."/spider_man.jpg";
+  echo "<img src='${img_path}' width=100 height=100>";
 ?>
     </html>
