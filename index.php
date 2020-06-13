bff3b407628e0f3c24e12f33c7be04cf655f778e
Ekansh jain <40785361+technicalej@users.noreply.github.com>
2020-06-13 23:56:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <p>hello this is my terraform html based file</p>
 <br>
 <?php
-  $cloudfront = "head -n1 path.txt";
+  $cloudfront = "head -n1 mydesti.txt";
   $img_path = "https://"./$cloudfront_url."/spider_man.jpg";
   echo "<img src='${img_path}' width="100" height="100">";
 ?>
